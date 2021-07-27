475e74707bbe5bef095911aadc62a695724f975f
ABHISHEK DAYAL <adayal487@gmail.com>
2021-07-28 03:05:07 +0530

    first commit

---
+++ src/Modal.svelte
@@ -0,0 +1,10 @@
+<script>
+ import {blur,slide,scale,fade,fly} from 'svelte/transition';
+ import {quintOut} from 'svelte/easing';
+</script>
+
+<div class="modal-container" in:blur out:fade>
+ <div class="modal-content" transition:fly={{y:200}}>
+   <slot><!--optional fallback --></slot>
+ </div>
+</div>
